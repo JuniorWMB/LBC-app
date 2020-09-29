eaa64698719b4a0e2648858f41eac8aefa989d86
Junior <juniormbaya243@gmail.com>
2020-09-29 19:57:34 +0200

    add component HeaderSearch and his css

--- src/Components/HeaderSearch.js
+++ src/Components/HeaderSearch.js
@@ -5,11 +5,41 @@ const HeaderSearch = () => {
     <div className="headersearch__contain">
       <div className="ellipse"></div>
       <div className="headersearch">
-        <div>
-          <input type="text" />
-          <button></button>
+        <div className="header__inputandbutton">
+          <input
+            className="header__input"
+            type="text"
+            placeholder="Que recherchez-vous ?"
+          />
+          <button className="header__button">Recherche</button>
         </div>
-        <div></div>
+        <div className="header__price">
+          <div className="header__priceleft">
+            <p>Prix entre</p>
+            <input
+              type="text"
+              placeholder="prix min"
+              className="header__pricevalue"
+            />
+            <p>et</p>
+            <input
+              type="text"
+              placeholder="prix max"
+              className="header__pricevalue"
+            />
+          </div>
+          <div>
+            <select
+              style={{
+                width: "220px",
+                height: "32px",
+              }}
+            >
+              <option value="récent ">Tri: Plus récentes </option>
+              <option value="ancien">Tri: Plus ancien</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   );
