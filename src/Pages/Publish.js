78178c0737a713b08c90bcc2d171a05d2aec8aed
Junior <juniormbaya243@gmail.com>
2020-10-04 18:03:23 +0200

    add a post publish

--- src/Pages/Publish.js
+++ src/Pages/Publish.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Cookies from "js-cookie";
 import "./publish.css";
+import axios from "axios";
 
 function Publish() {
   const [title, setTitle] = useState("Titre");
   const [text, setText] = useState("Texte de l'annonce");
   const [price, setPrice] = useState(0);
   const [file, setFile] = useState();
+  const userToken = Cookies.get("token");
+  const history = useHistory();
 
   const handleTtileChange = (e) => {
     setTitle(e.target.value);
@@ -19,6 +24,43 @@ function Publish() {
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!userToken) {
+      setTimeout(() => {
+        alert(
+          "Vous devez etre connecté pour déposer une annonce, vous allez etre dirigé vers la page login ..."
+        );
+        history.push("/login");
+      }, 2000);
+    } else {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("title", title);
+      formData.append("text", text);
+      formData.append("price", price);
+      try {
+        const response = await axios.post(
+          "https://leboncoin-api.herokuapp.com/offer/publish/",
+          formData,
+          {
+            headers: {
+              Authorization: "Beare" + userToken,
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+        history.push(`/offer/${response.data._id}`);
+      } catch (error) {
+        if (error.response.status === 500) {
+          console.error("An error occured");
+        } else {
+          console.error(error.response.data.msg);
+        }
+      }
+    }
+  };
   console.log("title is here>>", title);
   console.log("text is here>>", text);
   console.log("price is here>>", price);
@@ -30,7 +72,7 @@ function Publish() {
         <div className="publish__title">
           <h1>Déposer une annonce</h1>
         </div>
-        <form action="" className="publish__formblock">
+        <form action="" className="publish__formblock" onSubmit={handleSubmit}>
           <div className="publish__marginblock">
             <label htmlFor="titre">
               <p>Titre de l'annonce *</p>
